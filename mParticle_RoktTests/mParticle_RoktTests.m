cf5fb8d700563a96609cb861055dffa9add36384
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2025-06-10 17:38:22 -0400

    add unit test

--- mParticle_RoktTests/mParticle_RoktTests.m
+++ mParticle_RoktTests/mParticle_RoktTests.m
@@ -16,6 +16,8 @@
 - (NSDictionary<NSString *, NSString *> *) filteredUserAttributes:(NSDictionary<NSString *, NSString *> * _Nonnull)attributes kitConfiguration:(MPKitConfiguration *)kitConfiguration;
 
 - (void)addIdentityAttributes:(NSMutableDictionary<NSString *, NSString *> * _Nullable)attributes filteredUser:(FilteredMParticleUser * _Nonnull)filteredUser;
+
++ (RoktConfig *)convertMPRoktConfig:(MPRoktConfig *)mpRoktConfig;
     
 @end
 
@@ -276,6 +278,17 @@
     XCTAssertEqualObjects(passedAttributes[@"ios_idfv"], @"testVendorID");
     XCTAssertEqualObjects(passedAttributes[@"push_token"], @"testPushToken");
     XCTAssertEqualObjects(passedAttributes[@"device_application_stamp"], @"Test DAS");
+}
+
+- (void)testConvertMPRoktConfig {
+    MPRoktConfig *mpConfig = [[MPRoktConfig alloc] init];
+    mpConfig.colorMode = MPColorModeDark;
+    mpConfig.cacheDuration = @(100);
+    mpConfig.cacheAttributes = @{@"test": @"tested"};
+    
+    RoktConfig *roktConfig = [MPKitRokt convertMPRoktConfig:mpConfig];
+    
+    XCTAssert(roktConfig);
 }
 
 @end 
