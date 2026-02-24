0cf707fb70b501eb574596782452bb3c63caa4e1
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-24 15:25:06 -0500

    fix: remove tvOS platform declaration (#51)
    * fix: remove tvOS support
    
    * upgrade to ios 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "mParticle-Rokt",
-    platforms: [ .iOS(.v11), .tvOS(.v11) ], 
+    platforms: [ .iOS(.v12) ],
     products: [
         .library(
             name: "mParticle-Rokt",
